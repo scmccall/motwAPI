4e736b51b47740c340509c5f77a2417a32bf0b5a
Sam McCall <smccall@smccall.com>
2020-08-29 17:43:07 -0400

    Write tests for /playbooks GET

---
+++ test/playbooks.route.test.js
@@ -0,0 +1,15 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+
+describe("Playbooks route", () => {
+  it("should list ALL playbooks on /playbooks GET", () => {
+
+  });
+  it("should list a SINGLE playbook on /playbooks/:id", () => {
+
+  });
+  it("should list a SINGLE playbook on /playbooks/:name", () => {
+    
+  });
+});
