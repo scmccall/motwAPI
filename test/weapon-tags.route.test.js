dc58363c566ef33266fbf9ed7d72101783af2c27
Sam McCall <smccall@smccall.com>
2020-08-30 14:54:15 -0400

    Restructure weapon tags test

--- test/weapon-tags.route.test.js
+++ test/weapon-tags.route.test.js
@@ -31,27 +31,25 @@ describe("Weapon tags route", () => {
       });
     });
   });
-  describe("should list a SINGLE weapon tag on /weapon-tags/:index", () => {
-    it("request comes back in correct format", () => {
-      const tags = require("../data/weapon-tags/tags.json")
-      tags.forEach(tag => {
-        chai
-        .request(server)
-        .get(`/api/v1/weapon-tags/${tag.index}`)
-        .end((err, res) => {
-          // Returns successfully
-          expect(res).to.have.status(200);
-          expect(res).to.be.a.json;
-          expect(res.body).to.be.an('object');
+  it("should list a SINGLE weapon tag on /weapon-tags/:index", () => {
+    const tags = require("../data/weapon-tags/tags.json")
+    tags.forEach(tag => {
+      chai
+      .request(server)
+      .get(`/api/v1/weapon-tags/${tag.index}`)
+      .end((err, res) => {
+        // Returns successfully
+        expect(res).to.have.status(200);
+        expect(res).to.be.a.json;
+        expect(res.body).to.be.an('object');
 
-          // Returns in correct format
-          expect(res.body).to.have.property("index");
-          expect(res.body.index).to.be.a("string");
-          expect(res.body).to.have.property('name');
-          expect(res.body.name).to.be.a('string');
-          expect(res.body).to.have.property("description");
-          expect(res.body.description).to.be.a('string');
-        });
+        // Returns in correct format
+        expect(res.body).to.have.property("index");
+        expect(res.body.index).to.be.a("string");
+        expect(res.body).to.have.property('name');
+        expect(res.body.name).to.be.a('string');
+        expect(res.body).to.have.property("description");
+        expect(res.body.description).to.be.a('string');
       });
     });
   });
