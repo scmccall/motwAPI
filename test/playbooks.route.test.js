de019ee86ef7aa0ebeab1589ac92f4969c3698c8
Sam McCall <smccall@smccall.com>
2020-08-29 23:37:16 -0400

    Add "identifier" property to playbook objects

--- test/playbooks.route.test.js
+++ test/playbooks.route.test.js
@@ -6,33 +6,33 @@ const server = require("../server")
 chai.use(chaiHttp);
 
 describe("Playbooks route", () => {
-  describe("should list ALL playbooks on /playbooks GET", () => {
-    it("request comes back in proper format", () => {
-      chai
-      .request(server)
-      .get('/api/playbooks')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res).to.be.a.json;
-        expect(res.body).to.be.an('object');
-      });
-    });
-    it("request data is accurate", () => {
-      chai
-      .request(server)
-      .get('/api/playbooks')
-      .end((err, res) => {
-        expect(res.body).to.have.property("count");
-        expect(res.body).to.have.property("playbooks");
-        expect(res.body.count).to.equal(res.body.playbooks.length);
-        res.body.playbooks.forEach(element => {
-          expect(element).to.have.property("name");
-          expect(element.name).to.be.a('string');
-          expect(element).to.have.property("url");
-          expect(element.url).to.be.a('string');
-        });
+  it("should list ALL playbooks on /playbooks GET", () => {
+    chai
+    .request(server)
+    .get('/api/playbooks')
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.a.json;
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property("count");
+      expect(res.body).to.have.property("playbooks");
+      expect(res.body.count).to.equal(res.body.playbooks.length);
+      res.body.playbooks.forEach(element => {
+        expect(element).to.have.property("identifier");
+        expect(element.identifier).to.be.a("string");
+        expect(element).to.have.property("name");
+        expect(element.name).to.be.a('string');
+        expect(element).to.have.property("url");
+        expect(element.url).to.be.a('string');
       });
     });
   });
-  // describe("should list a SINGLE playbook on /playbooks/:name");
+  describe.skip("should list a SINGLE playbook on /playbooks/:name", () => {
+    it("request comes back in correct format", () => {
+
+    });
+    it("request data is accurate", () => {
+
+    });
+  });
 });
