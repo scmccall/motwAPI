72dc29c87553983f9169b1546f781078a6e34e20
Sam McCall <smccall@smccall.com>
2020-08-30 14:43:07 -0400

    Change properties in /playbooks res for clarity

--- test/playbooks.route.test.js
+++ test/playbooks.route.test.js
@@ -11,27 +11,31 @@ describe("Playbooks route", () => {
     .request(server)
     .get('/api/playbooks')
     .end((err, res) => {
+      // Returns in proper format
       expect(res).to.have.status(200);
       expect(res).to.be.a.json;
       expect(res.body).to.be.an('object');
+
+      // Returns correct data structure
       expect(res.body).to.have.property("count");
       expect(res.body).to.have.property("playbooks");
       expect(res.body.count).to.equal(res.body.playbooks.length);
       res.body.playbooks.forEach(element => {
-        expect(element).to.have.property("identifier");
-        expect(element.identifier).to.be.a("string");
+        expect(element).to.have.property("index");
+        expect(element.index).to.be.a("string");
         expect(element).to.have.property("name");
         expect(element.name).to.be.a('string');
         expect(element).to.have.property("url");
         expect(element.url).to.be.a('string');
+        expect(element.url).to.equal(`/${element.index}`);
       });
     });
   });
-  describe.skip("should list a SINGLE playbook on /playbooks/:name", () => {
+  describe("should list a SINGLE playbook on /playbooks/:index", () => {
     it("request comes back in correct format", () => {
 
     });
-    it("request data is accurate", () => {
+    it("shared playbook data has correct data structure", () => {
 
     });
   });
