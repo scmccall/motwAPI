58bbed94c959c7430c3fcc5629293f89f778d375
Sam McCall <smccall@smccall.com>
2020-09-08 19:49:38 -0400

    Remove test API route

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = express();
 // Add routes
 app.use("/api/v1", require("./routes/api"));
 
-app.get("/testurl", (req, res) => {
-  res.send("Test url reached!");
-});
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
